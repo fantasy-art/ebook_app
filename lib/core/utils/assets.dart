e957a1e31cc58b6c0f0549d57ab977199800fad4
يحيى حمود <fantasy-art@live.com>
2023-07-15 23:38:30 +0300

    First

---
+++ lib/core/utils/assets.dart
@@ -0,0 +1,4 @@
+class AssetsData {
+  static const logo = 'assets/images/ebook_logo.png';
+  static const testImage = 'assets/images/test_image.png';
+}
---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:ebook_app/constant.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+
+import 'features/splash/persentation/views/splash_view.dart';
+
+void main() {
+  runApp(const EBookApp());
+}
+
+class EBookApp extends StatelessWidget {
+  const EBookApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return GetMaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData().copyWith(
+        scaffoldBackgroundColor: kPrimeryColor,
+      ),
+      home: const SplashView(),
+    );
+  }
+}
