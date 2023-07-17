af450e09fe09b75510c9b0c6f6cb32e9e2063ccb
يحيى حمود <fantasy-art@live.com>
2023-07-17 03:34:43 +0300

    Create BookDetailView

---
+++ lib/features/home/prsenteation/views/book_datails_view.dart
@@ -0,0 +1,13 @@
+import 'package:ebook_app/features/home/prsenteation/views/widgets/book_detail_view_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: BookDetailViewBody(),
+    );
+  }
+}
