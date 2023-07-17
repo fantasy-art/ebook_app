fa93ade7b0a4445c4e8cd3931ef364f4c200af7b
يحيى حمود <fantasy-art@live.com>
2023-07-17 21:56:01 +0300

    add custom scroll view

--- lib/features/home/prsenteation/views/widgets/book_detail_view_body.dart
+++ lib/features/home/prsenteation/views/widgets/book_detail_view_body.dart
@@ -14,41 +14,45 @@ class BookDetailViewBody extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final width = MediaQuery.of(context).size.width;
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: [
-          const CustomBookDetailsAppBar(),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: width * 0.2),
-            child: const CustomBookImage(imageName: AssetsData.b1),
-          ),
-          const SizedBox(height: 43),
-          Text('Lost Legends',
-              style: Styles.textStyle24.copyWith(fontSize: 35)),
-          const SizedBox(height: 6),
-          Opacity(
-              opacity: 0.7,
-              child: Text('4 All-New Adventure',
-                  style: Styles.textStyle16
-                      .copyWith(fontStyle: FontStyle.italic))),
-          const SizedBox(height: 16),
-          const BookRating(),
-          const SizedBox(height: 37),
-          const BoxAction(),
-          const SizedBox(height: 37),
-          Align(
-            alignment: Alignment.centerLeft,
-            child: Text(
-              'You Can Also Like:',
-              style: Styles.textStyle14
-                  .copyWith(fontWeight: FontWeight.w600, color: Colors.white),
+    return CustomScrollView(slivers: [
+      SliverFillRemaining(
+          hasScrollBody: false,
+          child: Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 24),
+            child: Column(
+              children: [
+                const CustomBookDetailsAppBar(),
+                Padding(
+                  padding: EdgeInsets.symmetric(horizontal: width * 0.2),
+                  child: const CustomBookImage(imageName: AssetsData.b1),
+                ),
+                const SizedBox(height: 43),
+                Text('Lost Legends',
+                    style: Styles.textStyle24.copyWith(fontSize: 35)),
+                const SizedBox(height: 6),
+                Opacity(
+                    opacity: 0.7,
+                    child: Text('4 All-New Adventure',
+                        style: Styles.textStyle16
+                            .copyWith(fontStyle: FontStyle.italic))),
+                const SizedBox(height: 16),
+                const BookRating(),
+                const SizedBox(height: 37),
+                const BoxAction(),
+                const Expanded(child: SizedBox(height: 37)),
+                Align(
+                  alignment: Alignment.centerLeft,
+                  child: Text(
+                    'You Can Also Like:',
+                    style: Styles.textStyle14.copyWith(
+                        fontWeight: FontWeight.w600, color: Colors.white),
+                  ),
+                ),
+                const SizedBox(height: 12),
+                const FeatureSimilerListView(),
+              ],
             ),
-          ),
-          const SizedBox(height: 12),
-          const FeatureSimilerListView(),
-        ],
-      ),
-    );
+          )),
+    ]);
   }
 }
