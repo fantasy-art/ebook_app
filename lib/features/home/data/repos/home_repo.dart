b535157d06e9f2644d385b6860726d50165524e1
يحيى حمود <fantasy-art@live.com>
2023-07-18 22:26:44 +0300

    create home repo methods and failure file

--- lib/features/home/data/repos/home_repo.dart
+++ lib/features/home/data/repos/home_repo.dart
@@ -1,4 +1,9 @@
+import 'package:dartz/dartz.dart';
+import 'package:ebook_app/core/errors/failures.dart';
+import 'package:ebook_app/features/home/data/models/book_models/book_models.dart';
+
 abstract class HomeRepo {
-  fetchFeatureBooks();
-  fetchBestSellerBooks();
+  Future<Either<Failure, List<BookModels>>> fetchFeatureBooks();
+
+  Future<Either<Failure, List<BookModels>>> fetchBestSellerBooks();
 }
