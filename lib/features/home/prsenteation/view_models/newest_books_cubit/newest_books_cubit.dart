801bd722eefee193d26cff9dabb71eebf7a78b3d
يحيى حمود <fantasy-art@live.com>
2023-07-19 10:15:02 +0300

    Newest Books Cubit Done

--- lib/features/home/prsenteation/view_models/newest_books_cubit/newest_books_cubit.dart
+++ lib/features/home/prsenteation/view_models/newest_books_cubit/newest_books_cubit.dart
@@ -2,8 +2,20 @@ import 'package:ebook_app/features/home/data/models/book_models/book_models.dart
 import 'package:equatable/equatable.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 
+import '../../../data/repos/home_repo.dart';
+
 part 'newest_books_state.dart';
 
 class NewestBooksCubit extends Cubit<NewestBooksState> {
-  NewestBooksCubit() : super(NewestBooksInitial());
+  NewestBooksCubit(this.homeRepo) : super(NewestBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchNewestBooks() async {
+    emit(NewestBooksLoading());
+    var result = await homeRepo.fetchNewestBooks();
+
+    result.fold((failure) => emit(NewestBooksFailure(failure.errMessage)),
+        (books) => emit(NewestBooksSuccess(books)));
+  }
 }
