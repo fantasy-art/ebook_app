6c98f60e1ae6cba1230d73c36a63e6e88f6f5963
يحيى حمود <fantasy-art@live.com>
2023-07-19 09:51:33 +0300

    create featured books cubit state

---
+++ lib/features/home/prsenteation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -0,0 +1,9 @@
+import 'package:bloc/bloc.dart';
+import 'package:ebook_app/features/home/data/models/book_models/book_models.dart';
+import 'package:equatable/equatable.dart';
+
+part 'featured_books_state.dart';
+
+class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
+  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+}
