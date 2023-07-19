f225880334563bec7102420f5bcefeea97efa035
يحيى حمود <fantasy-art@live.com>
2023-07-19 10:04:31 +0300

    create feature books method

--- lib/features/home/prsenteation/view_models/featured_books_cubit/featured_books_cubit.dart
+++ lib/features/home/prsenteation/view_models/featured_books_cubit/featured_books_cubit.dart
@@ -1,3 +1,4 @@
+import 'package:ebook_app/features/home/data/repos/home_repo.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:ebook_app/features/home/data/models/book_models/book_models.dart';
 import 'package:equatable/equatable.dart';
@@ -5,5 +6,15 @@ import 'package:equatable/equatable.dart';
 part 'featured_books_state.dart';
 
 class FeaturedBooksCubit extends Cubit<FeaturedBooksState> {
-  FeaturedBooksCubit() : super(FeaturedBooksInitial());
+  FeaturedBooksCubit(this.homeRepo) : super(FeaturedBooksInitial());
+
+  final HomeRepo homeRepo;
+
+  Future<void> fetchFeaturedBooks() async {
+    emit(FeaturedBooksLoading());
+    var result = await homeRepo.fetchFeatureBooks();
+
+    result.fold((failure) => emit(FeaturedBooksFailure(failure.errMessage)),
+        (books) => emit(FeaturedBooksSuccess(books)));
+  }
 }
