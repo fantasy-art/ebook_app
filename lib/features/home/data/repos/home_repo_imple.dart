343336e94e80bdac99abc2b2f93c6f9f274d9713
يحيى حمود <fantasy-art@live.com>
2023-07-19 08:36:33 +0300

    implement fetch newest books

--- lib/features/home/data/repos/home_repo_imple.dart
+++ lib/features/home/data/repos/home_repo_imple.dart
@@ -1,18 +1,31 @@
 import 'package:dartz/dartz.dart';
 import 'package:ebook_app/core/errors/failures.dart';
+import 'package:ebook_app/core/utils/api_service.dart';
 import 'package:ebook_app/features/home/data/models/book_models/book_models.dart';
 import 'package:ebook_app/features/home/data/repos/home_repo.dart';
 
 class HomeRepoImpl implements HomeRepo {
+  final ApiService apiService;
+
+  HomeRepoImpl(this.apiService);
   @override
-  Future<Either<Failure, List<BookModels>>> fetchBestSellerBooks() {
-    // TODO: implement fetchBestSellerBooks
-    throw UnimplementedError();
+  Future<Either<Failure, List<BookModels>>> fetchNewestBooks() async {
+    try {
+      var data = await apiService.get(
+          endPoint:
+              'volumes?Filtering=free-ebooks&Sorting=newest &q=programming');
+      List<BookModels> books = [];
+      for (var item in data['items']) {
+        books.add(BookModels.fromJson(item));
+      }
+      return right(books);
+    } catch (e) {
+      return left(ServerFailure());
+    }
   }
 
   @override
   Future<Either<Failure, List<BookModels>>> fetchFeatureBooks() {
-    // TODO: implement fetchFeatureBooks
     throw UnimplementedError();
   }
 }
